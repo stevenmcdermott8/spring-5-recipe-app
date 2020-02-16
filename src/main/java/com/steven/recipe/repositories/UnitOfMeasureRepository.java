3c37c9f128139c338de486c8630ff679305eb77c
Steven McDermott <stevmc@vsp.com>
2020-02-16 14:25:00 -0800

    Adding query by description for category and unit of measure

--- src/main/java/com/steven/recipe/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/steven/recipe/repositories/UnitOfMeasureRepository.java
@@ -3,5 +3,9 @@ package com.steven.recipe.repositories;
 import com.steven.recipe.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+	Optional<UnitOfMeasure> findByDescription(String description);
 }
