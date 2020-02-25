ce5a7ca6ef8980cf3bf0042bd2ff630c9bdd2cd2
Steven McDermott <stevenmcdermott8@gmail.com>
2020-02-25 07:17:44 -0800

    adding a mock mvc test for reference

--- src/test/java/com/steven/recipe/controllers/IndexControllerTest.java
+++ src/test/java/com/steven/recipe/controllers/IndexControllerTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -15,6 +17,9 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 class IndexControllerTest {
 
@@ -28,6 +33,12 @@ class IndexControllerTest {
 	void setUp() {
 		MockitoAnnotations.initMocks(this);
 		controller = new IndexController(recipeService);
+	}
+
+	@Test
+	void testMockMVC() throws Exception {
+		MockMvc mockMVC = MockMvcBuilders.standaloneSetup(controller).build();
+		mockMVC.perform(get("/")).andExpect(status().isOk()).andExpect(view().name("index"));
 	}
 
 	@Test
@@ -57,4 +68,5 @@ class IndexControllerTest {
 		Set<Recipe> setInController = argumentCaptor.getValue();
 		assertEquals(2, setInController.size());
 	}
+
 }
